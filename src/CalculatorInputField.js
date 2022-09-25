548fa71f76ec1986e346a4c56b7c788eecfeec8f
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2022-09-25 15:59:25 +0100

    Add support for operands with different bases

--- src/CalculatorInputField.js
+++ src/CalculatorInputField.js
@@ -5,21 +5,40 @@ import Number from "./Number.js";
 class CalculatorInputField extends React.Component {
 	constructor(props) {
 		super(props);
+
+		this.state = { base: 2 };
+
 		this.handleChange = this.handleChange.bind(this);
 	}
 
 	handleChange(e) {
-		var number = new Number(e.target.value, 2);
-		this.props.action(this.props.id, number.getValue());
+		if (e.target.name === "number") {
+			var number = new Number(e.target.value, this.state.base).getValue();
+			this.props.action(this.props.id, number);
+		} else if (e.target.name === "base") {
+			document.getElementsByName("number")[this.props.id - 1].value = "";
+			this.setState({ base: e.target.value });
+		}
 	}
 
 	render() {
 		return (
-			<input
-				className="inputField"
-				maxlength="39"
-				onChange={this.handleChange}
-			></input>
+			<div className="inputField">
+				<input
+					className="numberInputField"
+					name="number"
+					onChange={this.handleChange}
+				></input>
+				<select
+					className="baseInputField"
+					name="base"
+					onChange={this.handleChange}
+				>
+					<option>2</option>
+					<option>16</option>
+					<option>10</option>
+				</select>
+			</div>
 		);
 	}
 }
